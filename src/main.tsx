ff9c40bcff197ec6dc5754f03b7081591b2635e8
thetarnav <gthetarnav@gmail.com>
2022-07-16 12:47:04 +0200

    1 file modified, 1 deleted

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-/* @refresh reload */
-import { enableScheduling } from 'solid-js';
-import { render } from 'solid-js/web';
-
-import App from './App';
-
-enableScheduling();
-
-render(() => <App />, document.getElementById('root') as HTMLElement);
